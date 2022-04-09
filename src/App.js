f5ecfc891bb563bdcf54e5a1db4ff24af41545f9
akramHoque <96454691+akramHoque@users.noreply.github.com>
2022-04-09 17:35:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
+import { createUserWithEmailAndPassword, getAuth,signInWithEmailAndPassword,sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 import './App.css';
+import app from "./firebase.init";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { useState } from "react";
 
+const auth = getAuth(app)
 function App() {
+  const [validated, setValidated] = useState(false);
+  const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState('')
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleEmailBlur = event => {
+    setEmail(event.target.value);
+  }
+
+
+  const handlePasswordBlur = event => {
+    setPassword(event.target.value);
+  }
+
+  const handleRegisterdChange = event => {
+    setRegistered(event.target.checked) ;
+  }
+ const  handlePasswordReset = () => {
+   sendPasswordResetEmail(auth, email)
+   .then(() => {
+     console.log('email sent') ;
+   })
+ }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+      return;
+    }
+
+    if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+      setError('Password Should contain at least one special character');
+      return;
+    }
+    setValidated(true);
+    setError('')
+    if(registered){
+      signInWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        console.log(user) ;
+      })
+      .catch(error => {
+        console.error(error) ;
+        setError(error.message);
+      })
+    }
+    else{
+      createUserWithEmailAndPassword(auth, email, password)
+      .then(result => {
+        const user = result.user;
+        setEmail('');
+        setPassword('');
+        verifyEmail() ;
+      })
+      .catch(error => {
+        console.error(error);
+        setError(error.message)
+      })
+    }
+    
+    event.preventDefault();
+
+  }
+
+  const verifyEmail = () =>{
+   sendEmailVerification(auth.currentUser)
+   .then(() => {
+     console.log('verification sent')
+   })
+
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="registration w-50 mx-auto">
+        <h2 className="text-primary mt-5"> Please { registered ? 'Login':  'Register'}!!! </h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmailBlur} required type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label >Password</Form.Label>
+            <Form.Control onBlur={handlePasswordBlur} required type="password" placeholder="Password" />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check onChange={handleRegisterdChange} type="checkbox" label="Already registered?" />
+          </Form.Group>
+          <p className="text-danger">{error}</p>
+          <Button onClick = {handlePasswordReset} variant="link">Forget Password?</Button>
+          <br />
+          <Button variant="primary" type="submit">
+           {registered ? 'Login' : 'Register'}
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhUKOBm_r5skSjDP5hvopoZzJu8XhBx3w",
+  authDomain: "email-pass-auth-8ad31.firebaseapp.com",
+  projectId: "email-pass-auth-8ad31",
+  storageBucket: "email-pass-auth-8ad31.appspot.com",
+  messagingSenderId: "575787400652",
+  appId: "1:575787400652:web:c91e28c7345f96e1ceb169"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app ;
